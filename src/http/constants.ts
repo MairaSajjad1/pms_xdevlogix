3195e795bd87c25f7eb222ad666cfc4ff9ffab03
MairaSajjad <mairasajjad2001@gmail.com>
2023-10-31 09:49:39 +0500

    updated

--- src/http/constants.ts
+++ src/http/constants.ts
@@ -1,2 +1,5 @@
 export const GET_LEASE = "api/leases";
 export const GET_LEASE_INSTALLMENT = "api/lease-rent-installments/";
+export const GET_LANDLORD ="api/landlords/"
+export const GET_TENANTS= "api/tenants/"
+export const GET_SOURCE = "api/sources"
